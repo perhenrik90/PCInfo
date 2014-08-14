f886f8c44c6a67d34545de4f0325f9b81d482c06
Per-Henrik Kvalnes <perhenrik@nordlandssykehuset.no>
2014-08-14 09:38:00 +0200

    added some css, post form on index.php and started on submit.html

---
+++ lang/no-nb.php
@@ -0,0 +1,12 @@
+<?php
+/***************************************
+ * Strings for norwegian translation
+ * 
+ * @author Per-Henrik Kvalnes
+ ****************************************/
+
+$_lang["header"] = "Registrer opplysninger";
+$_lang["name"] = "Navn";
+$_lang["email"] = "Epost";
+
+?>
