6a23166417fdb334906ec58154efe77ee5c86ca7
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2014-09-01 20:38:20 +0200

    added english php file

---
+++ lang/en.php
@@ -0,0 +1,23 @@
+<?php
+/***************************************
+ * Strings for english translation
+ * 
+ * @author Per-Henrik Kvalnes
+ ****************************************/
+
+/** strings for buttons **/
+$_lang["submit"] = "Submit";
+
+/** strings for fields ***/
+$_lang["header"] = "Register information";
+$_lang["name"] = "Name";
+$_lang["email"] = "Email";
+
+$_lang["os"] = "OS";
+$_lang["browser"] = "Browser";
+$_lang["screen"] = "Screen Resolution";
+$_lang["flash"] = "Flash";
+
+/** strings for sendt mail **/
+$_lang["mailsendt"] = "You have recorded the following information";
+?>
